6be9e481edbd8fbf49d43aa543e44625c97574c0
markusmayr <mayrmarkus-@outlook.com>
2018-09-07 10:10:01 +0200

    sdfg

---
+++ Sortieren/src/sortieren/Sortieren.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortieren;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Markus_Mayr
+ */
+public class Sortieren {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] unsortiert = new int [5];
+        generierenrand();
+        bubblesort();
+    }
+
+    private static void bubblesort() {
+        
+    }
+
+    private static void generierenrand() {
+        Random r = new Random();
+        int randomzahl = rand.nextint()
+    }
+    
+}
