2a152595a6cf7f1ad00fa965fcf5cdc616ac6634
Unknown <mayrmarkus-@outlook.com>
2018-09-20 09:44:07 +0200

    bubble

--- Datenstruckturen/src/datenstruckturen/LinkedList.java
+++ Datenstruckturen/src/datenstruckturen/LinkedList.java
@@ -28,9 +28,11 @@ public class LinkedList {
     public void printLinkedList(){
         Node temp = first;
         while(temp != null){
-            System.out.println(temp.getZahl());
+            System.out.print(temp.getZahl());
+            System.out.print(", ");
             temp = temp.getNext();
         }
+        System.out.println("");
     }
     
     public void deleteNode(Node n){
@@ -66,14 +68,24 @@ public class LinkedList {
     }
     
     public void addNodePlace(int a, Node n){
+        
         Node temp = first;
         Node previous = null;
-        for (int i = 1; i <=a; i++) {
-            previous = temp;
-            temp = temp.getNext();
+        if(a>nodeSize())
+            addNode(n);
+        else{
+            if(a == 1)
+                addBeginning(n);
+            else{
+                for (int i = 1; i < a; i++) {
+                    previous = temp;
+                    temp = temp.getNext();
+                }
+            previous.setNext(n);
+            n.setNext(temp);    
+            }
         }
-        temp.setNext(n);    
-        }
+    }
         
     public int nodeSize(){
         int length = 0;
@@ -84,6 +96,32 @@ public class LinkedList {
         }
         return length;
     }
+    
+    public Node stelleX(int a){
+        Node temp = first;
+        int count = 0;
+        while(temp != null){
+            if(count == a)
+                return temp;
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+    
+    public void sort(){
+        for(int i=nodeSize() -1; i>0; i--){
+            for(int j=0; j<nodeSize()-1; j++ ){
+                if(stelleX(j)>stelleX(j+1)){
+                    int temp = stelleX(j+1);
+                    stelleX(j+1) = stelleX(j);
+                    stelleX(j) = temp;
+                }
+            
+            }
+        
+        }
+    }
+            
 
     public static void main(String[]args){
         Node strat = new Node(888);
@@ -91,6 +129,7 @@ public class LinkedList {
         Node m2 = new Node(444);
         Node m3 = new Node(222);
         Node m4 = new Node(100);
+        Node m5 = new Node(5555);
         
         LinkedList ll = new LinkedList(strat);
         ll.addNode(m1);
@@ -98,7 +137,8 @@ public class LinkedList {
         ll.addNode(m3);
         ll.addNode(m4);
         
-        ll.deleteNodePlace(0);
+        ll.addNodePlace(20, m5);
+        //ll.deleteNodePlace(0);
         ll.printLinkedList();
         
         System.out.println(ll.nodeSize());
