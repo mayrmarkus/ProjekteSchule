27b286046f46478a0b2926acb5951b3a2c26e633
markusmayr <mayrmarkus-@outlook.com>
2018-09-24 12:25:03 +0200

    Hash

--- Datenstruckturen/src/datenstruckturen/Hashtable.java
+++ Datenstruckturen/src/datenstruckturen/Hashtable.java
@@ -11,5 +11,27 @@ package datenstruckturen;
  */
 public class Hashtable {
     
+    LinkedList[] tabelle = new LinkedList[15];
     
+    public void addValue(int wert){
+        int h = wert%15;
+        Node n = new Node(wert);
+        
+        if(tabelle[h] == null)
+            tabelle[h] = new LinkedList(n);
+        else
+            tabelle[h].addNode(n);
+    }
+        
+    public void printHashTable(){
+        for (int i = 0; i < 15; i++) {
+            LinkedList ll = tabelle[i];
+            System.out.print(i+"\t");
+            if(ll != null)
+                ll.printLinkedList();
+            else
+                System.out.println("");
+        }
+        
+    }
 }
--- Datenstruckturen/src/datenstruckturen/main.java
+++ Datenstruckturen/src/datenstruckturen/main.java
@@ -11,32 +11,37 @@ package datenstruckturen;
  */
 public class main {
     public static void main(String[]args){
-//        Node start = new Node(888);
-//        Node m1 = new Node(666);
-//        Node m2 = new Node(444);
-//        Node m3 = new Node(222);
-//        Node m4 = new Node(100);
-//        Node m5 = new Node(5555);
-//        
-//        LinkedList ll = new LinkedList(start);
+        Node start = new Node(888);
+        Node m1 = new Node(666);
+        Node m2 = new Node(444);
+        Node m3 = new Node(222);
+        Node m4 = new Node(100);
+        Node m5 = new Node(5555);
+ 
+       LinkedList ll = new LinkedList(start);
 //        ll.addNode(m1);
 //        ll.addNode(m2);
 //        ll.addNode(m3);
 //        ll.addNode(m4);
-//        
+        
 //        ll.addNodePlace(20, m5);
 //        //ll.deleteNodePlace(0);
 //        ll.printLinkedList();
 //        ll.sort();
 //        ll.printLinkedList();
 //        System.out.println(ll.nodeSize());
+        Hashtable aa = new Hashtable();
+        aa.addValue(8);
+        aa.addValue(18);
+        aa.addValue(88);
+        aa.addValue(99);
+        aa.addValue(30);
+        aa.addValue(79);
+        aa.addValue(45);
         
-        SortedLinkedList sll = new SortedLinkedList(new Node(20));
-        sll.addNode(new Node(5));
-        sll.addNode(new Node(2));
-        sll.addNode(new Node(2));
-        sll.addNode(new Node(1));
-        sll.addNode(new Node(1));
-        sll.printLinkedList();
+        aa.printHashTable();
+        
+        
+        
     }
 }
