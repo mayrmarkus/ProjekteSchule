90c790612a36c675428228df74c7091750fe0bf9
mayrmarkus <43027477+mayrmarkus@users.noreply.github.com>
2019-01-07 14:09:17 +0100

    Update BallWorld.java

--- BouncingBallAdvanced/src/bouncingballadvanced/BallWorld.java
+++ BouncingBallAdvanced/src/bouncingballadvanced/BallWorld.java
@@ -8,6 +8,8 @@ package bouncingballadvanced;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -20,13 +22,16 @@ public class BallWorld extends JFrame{
     private BallCage cage;
     private DrawPanel panel;
     private ArrayList<Ball> baelle;
+    private JButton bt_add;
     
-    public BallWorld(int h, int w){
+    public BallWorld(int w, int h){
+    	
+    	baelle = new ArrayList<>();
         
-        this.setSize(h,w);
+        this.setSize(w,h);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        cage = new BallCage(h, w, Color.BLACK, Color.orange);
+        cage = new BallCage(w, h, Color.BLACK, Color.orange);
         panel = new DrawPanel(w, h, cage);
         
         this.setLayout(new BorderLayout());
@@ -43,6 +48,10 @@ public class BallWorld extends JFrame{
         Ball b3 = new Ball(75, 75, 5, 5, 7, Color.orange);
         baelle.add(b3);
         panel.addBall(b3);
+        
+        bt_add = new JButton();
+        bt_add.setSize(50,30);
+        bt_add.setText("ADD");
     
     }
     
@@ -68,6 +77,5 @@ public class BallWorld extends JFrame{
         for(int i=0;i<baelle.size();i++){
              baelle.get(i).move(cage);
          }
-    
     }
 }
