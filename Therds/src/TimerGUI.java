d3a73b30143976299ab9f233aed2f99b65aa4b41
mayrmarkus <43027477+mayrmarkus@users.noreply.github.com>
2019-02-11 14:10:15 +0100

    Update TimerGUI.java

--- Therds/src/TimerGUI.java
+++ Therds/src/TimerGUI.java
@@ -1,5 +1,8 @@
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,16 +20,21 @@ import javax.swing.JPanel;
  */
 public class TimerGUI extends JFrame{
     
+	
+	private boolean status;
+	private boolean reset;
+	
     JPanel panel;
     JLabel output;
-    JButton button;
+    JButton button[] = new JButton[3];
+    
     
     public TimerGUI(){
         initialize();
     }
     private void initialize(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(200, 200);
+        this.setSize(317, 200);
         
         panel = new JPanel();
         panel.setLayout(null);
@@ -38,15 +46,51 @@ public class TimerGUI extends JFrame{
         output.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
         panel.add(output);
         
-        button = new JButton();
-        button.setSize(80, 50);
-        button.setText("Start");
-        panel.add(button);
+        for(int i = 0; i < button.length; i++){
+        button[i] = new JButton();
+        button[i].setBounds(i*100, 1, 100, 50);
+        panel.add(button[i]);
+        }
+        button[0].setText("Start");
+        button[1].setText("Reset");
+        button[2].setText("End");
         
-        button.addActionListener(null);
+        button[0].addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				status = true;
+			}
+		});
+        
+        button[1].addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				reset = true;
+			}
+		});
+        
+        button[2].addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				status = false;
+				
+			}
+		});
     }
     
-    public void setOutput(String minuten, String sekunden, String ms){
+	protected boolean setReset() {
+		return reset;
+		
+	}
+	
+	public void setOutput(String minuten, String sekunden, String ms){
         output.setText(minuten + ":" + sekunden + ":" + ms);
+    }
+    
+    public boolean getStatus(){
+    	return status;
     }
 }
